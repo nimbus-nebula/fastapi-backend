bd373f7ec39072fa233eedb8ad48d7f899522d0d
Justyn Lantz <jtlantz1995@gmail.com>
2023-03-13 21:19:20 +0700

    fix some s3 imports being weird

---
+++ src/s3/config.py
@@ -0,0 +1,15 @@
+import os
+
+s3_user = os.environ.get("S3_USER")
+s3_password = os.environ.get("S3_PASSWORD")
+s3_host = os.environ.get("S3_HOST")
+s3_port = os.environ.get("S3_PORT")
+
+
+s3_config = {
+    "service_name": "s3",
+    "aws_access_key_id": s3_user,
+    "aws_secret_access_key": s3_password,
+    "endpoint_url": f"http://{s3_host}:{s3_port}",
+    "verify": False
+}
--- src/s3/filesto.py
+++ src/s3/filesto.py
@@ -2,22 +2,7 @@ import os
 import boto3
 
 from datetime import datetime
-
-s3_user = os.environ.get("S3_USER")
-s3_password = os.environ.get("S3_PASSWORD")
-s3_host = os.environ.get("S3_HOST")
-s3_port = os.environ.get("S3_PORT")
-# TODO: Need to find a more dynamic way to retrieve the bucket
-s3_bucket = os.environ.get("S3_BUCKET")
-
-
-s3_config = {
-    "service_name": "s3",
-    "aws_access_key_id": s3_user,
-    "aws_secret_access_key": s3_password,
-    "endpoint_url": f"http://{s3_host}:{s3_port}",
-    "verify": False
-}
+from src.s3.config import s3_config
 
 s3 = boto3.resource(**s3_config)
 
@@ -31,7 +16,8 @@ def upload_file(file: any) -> str:
     """
 
     try:
-        bucket = s3_bucket
+        # TODO: Need to find a more dynamic way to retrieve the bucket
+        bucket = os.environ.get("S3_BUCKET")
         timestamp = datetime.utcnow().isoformat()
         filetype = file.filename.split('.')[-1]
         filename = f"{timestamp}{filetype}"
@@ -44,4 +30,4 @@ def upload_file(file: any) -> str:
 
 
 def get_public_s3_url(filename: str) -> str:
-    return f"http://{s3_host}:{s3_port}/card-images/{filename}"
+    return f"{s3_config['endpoint_url']}/card-images/{filename}"
